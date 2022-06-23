d2dfa3354c2cefa98670bb06028da7c087cdff52
xehn1337 <100049731+xehn1337@users.noreply.github.com>
2022-06-23 21:08:59 +0200

    release

---
+++ TJprojMain-remover/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TJprojMain_remover
+{
+    class Program
+    {
+        private const string AUTOSTART_REGKEY = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run";
+        private const string AUTOSTART_REGKEY2 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\RunOnce";
+
+        static void Main(string[] args)
+        {
+            Log.Info("TJprojMain-remover by xehn1337", ConsoleColor.Blue);
+            Console.WriteLine();
+
+            CheckSafeBoot();
+            DisableAutostart();
+
+            RemoveFiles();
+
+            Log.Info("Done.");
+            Console.ReadLine();
+        }
+
+        static void CheckSafeBoot()
+        {
+            var safeBoot = Utils.IsSafeMode();
+            if (!safeBoot)
+            {
+                Log.Critical("You did not boot into safe mode, which means that the processes cannot be deleted.");
+                Log.Info("Here is how to boot into safe mode: https://www.digitalcitizen.life/4-ways-boot-safe-mode-windows-10/");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        static bool DisableAutostart()
+        {
+            Log.Info("Removing autostart registry keys...");
+            Console.WriteLine();
+            bool success = false;
+            try
+            {
+              
+                success |= Utils.RegRemoveIfExists(AUTOSTART_REGKEY, "svchost");
+                success |= Utils.RegRemoveIfExists(AUTOSTART_REGKEY, "Explorer");
+
+                success |= Utils.RegRemoveIfExists(AUTOSTART_REGKEY2, "svchost");
+                success |= Utils.RegRemoveIfExists(AUTOSTART_REGKEY2, "Explorer");
+                Console.WriteLine();
+                
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Failed to remove autostart keys: " + ex.Message);
+            }
+            if (success)
+            {
+                Log.Info("Removed Autostart keys successfully!");
+            }
+            return success;
+        }
+
+        static void RemoveFiles()
+        {
+            Console.WriteLine();
+            try
+            {
+                Utils.Unhide(@"C:\Windows\Resources\*.*");
+                Utils.Unhide(@"C:\Windows\Resources\Themes\*.*");
+
+
+                Utils.FRemoveIfExists(@"C:\Windows\Resources\svchost.exe");
+                Utils.FRemoveIfExists(@"C:\Windows\Resources\spoolsv.exe");
+                Utils.FRemoveIfExists(@"C:\Windows\Resources\Themes\explorer.exe");
+                Utils.FRemoveIfExists(@"C:\Windows\Resources\Themes\icsys.icn.exe");
+                Utils.FRemoveIfExists(@"C:\Windows\Resources\Themes\icsys.icn");
+                Utils.FRemoveIfExists(@"C:\Windows\Resources\Themes\tjcm.cmn");
+                Console.WriteLine();
+
+            }catch(Exception ex)
+            {
+                Log.Error("Could not remove files: " + ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ TJprojMain-remover/Utils.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TJprojMain_remover
+{
+    public class Utils
+    {
+        private const int SM_CLEANBOOT = 67;
+
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int smIndex);
+
+        public static bool IsSafeMode()
+        {
+            return GetSystemMetrics(SM_CLEANBOOT) != 0;
+        }
+
+        public static bool RegRemoveIfExists(string key, string name)
+        {
+            using (RegistryKey regKey = Registry.LocalMachine.OpenSubKey(key, writable: true))
+            {
+                if (regKey != null)
+                {
+                    if (regKey.GetValue(name) != null)
+                    {
+                        regKey.DeleteValue(name);
+                        Log.Critical($"Registry key {name} found and autostart entry removed!");
+                        return true;
+                    }
+                    else
+                    {
+                        Log.Error($"Registry key {name} not found, searching elsewhere...");
+                        return false;
+                    }
+                }
+                else
+                {
+                    Log.Error("Registry Key not found!");
+                    return false;
+                }
+            }
+
+        }
+
+        public static void Unhide(string path)
+        {
+            Process.Start("attrib", $"-r -a -s -h \"{path}\"");
+        }
+
+        public static void FRemoveIfExists(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    Log.Critical($"Removed {path} successfully!");
+                }
+                else
+                {
+                    Log.Error($"File {path} not found!");
+                }
+            }catch(Exception ex)
+            {
+                Log.Error($"Could not delete file {path}: {ex.Message}");
+            }
+        }
+    }
+}
